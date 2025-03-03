cfa2ec6f78823b9075aa5f9e4564495aaad75378
Davi Souza <davissouza2021@gmail.com>
2025-03-03 19:58:28 -0300

    initial commit

---
+++ server/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen(); 
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(); 
+    app.UseSwaggerUI(c => 
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API V1");
+        c.RoutePrefix = string.Empty; 
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
